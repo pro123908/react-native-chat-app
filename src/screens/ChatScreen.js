d433daeea42461a096c9f5beedebcb301d8d36c3
Bilal Ahmad <pro123908@gmail.com>
2020-10-25 21:19:23 +0500

    update

--- src/screens/ChatScreen.js
+++ src/screens/ChatScreen.js
@@ -8,49 +8,95 @@ import {
   Platform,
 } from 'react-native';
 import {GiftedChat} from 'react-native-gifted-chat';
-import Firebase from '../../Firebase';
+import Fire from '../../Firebase';
 
-const ChatScreen = ({navigation}) => {
-  console.log('Navigation => ', navigation);
-  const [messages, setMessages] = useState([]);
-
-  const user = () => {
-    return {
-      _id: Firebase.uid,
-      name: navigation.state.params.name,
-    };
+export default class ChatScreen extends React.Component {
+  state = {
+    messages: [],
   };
 
-  useEffect(() => {
-    Firebase.get((message) =>
-      setMessages((previous) => ({
+  get user() {
+    return {
+      _id: Fire.uid,
+      name: this.props.route.params.name,
+    };
+  }
+  componentDidMount() {
+    Fire.get((message) =>
+      this.setState((previous) => ({
         messages: GiftedChat.append(previous.messages, message),
       })),
     );
-
-    return () => {
-      Firebase.off();
-    };
-  }, []);
-
-  const chat = (
-    <GiftedChat messages={messages} onSend={Firebase.send} user={user} />
-  );
-
-  if (Platform.OS === 'android') {
-    return (
-      <KeyboardAvoidingView
-        style={{flex: 1}}
-        behavior="padding"
-        keyboardVerticalOffset={30}
-        enabled>
-        {chat}
-      </KeyboardAvoidingView>
-    );
   }
-  return <SafeAreaView style={{flex: 1}}>{chat}</SafeAreaView>;
-};
 
-export default ChatScreen;
+  componentWillUnmount() {
+    Fire.off();
+  }
+
+  render() {
+    const chat = (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={Fire.send}
+        user={this.user}
+      />
+    );
+    if (Platform.OS === 'android') {
+      return (
+        <KeyboardAvoidingView
+          style={{flex: 1}}
+          behavior="padding"
+          keyboardVerticalOffset={30}
+          enabled>
+          {chat}
+        </KeyboardAvoidingView>
+      );
+    }
+    return <SafeAreaView style={{flex: 1}}>{chat}</SafeAreaView>;
+  }
+}
+
+// const ChatScreen = ({navigation}) => {
+//   console.log('Navigation => ', navigation);
+//   const [messages, setMessages] = useState([]);
+
+//   const user = () => {
+//     return {
+//       _id: Fire.uid,
+//       name: navigation.state.params.name,
+//     };
+//   };
+
+//   useEffect(() => {
+//     Fire.get((message) =>
+//       setMessages((previous) => ({
+//         messages: GiftedChat.append(previous.messages, message),
+//       })),
+//     );
+
+//     return () => {
+//       Fire.off();
+//     };
+//   }, []);
+
+//   const chat = (
+//     <GiftedChat messages={messages} onSend={Fire.send} user={user} />
+//   );
+
+//   if (Platform.OS === 'android') {
+//     return (
+//       <KeyboardAvoidingView
+//         style={{flex: 1}}
+//         behavior="padding"
+//         keyboardVerticalOffset={30}
+//         enabled>
+//         {chat}
+//       </KeyboardAvoidingView>
+//     );
+//   }
+//   return <SafeAreaView style={{flex: 1}}>{chat}</SafeAreaView>;
+// };
+
+// export default ChatScreen;
 
 const styles = StyleSheet.create({});
